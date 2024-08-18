e64a7942c7fef86c3fbb0d5b7bb445aef4c19d7e
Nileshsaha12 <nileshsaha264@gmail.com>
2024-08-18 08:10:57 +0530

    print 1 to n

---
+++ Recursion/print1toN.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class print1toN {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter N");
+        int n=sc.nextInt();
+        printn(1,n);
+    }
+    public static void printn(int i,int n){
+        if(i>n) return;
+        System.out.println(i);
+        printn(i+1, n);
+    }
+}
