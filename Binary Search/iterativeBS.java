d0238ab3d49a2dd12934942a6cfb77c53c00a677
Nileshsaha12 <nileshsaha264@gmail.com>
2024-12-13 10:29:54 +0530

    Binary Search in Iterative way..TC=O(nlogn) and SC=O(1)

---
+++ Binary Search/iterativeBS.java
@@ -0,0 +1,28 @@
+public class iterativeBS {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7,8,9};
+        int x=10;
+        iter(arr,x);
+    }
+    public static void iter(int[] arr,int x){
+        int n=arr.length;
+        int low=0;
+        int fl=1;
+        int high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==x){
+                fl=0;
+                System.out.println(mid);
+                break;
+            }
+            else if(arr[mid]<x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        if(fl==1) System.out.println("Not found");
+    }
+}
